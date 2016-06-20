bdc6af6179b28813a678d625b50a82aad328af60
Lizzy <lizzyyang9534@yahoo.com.tw>
2016-06-20 21:17:01 +0800

    新增/修改食譜:ingredient新增&修改

--- api/recipe_edit.php
+++ api/recipe_edit.php
@@ -11,7 +11,6 @@ $recipe_content = mysql_real_escape_string($data->recipe_content);
 $recipe_ingredients = json_decode(json_encode($data->recipe_ingredients), true);
 
 try{
-  /*
 	$sql = "update recipe set recipe_title = :recipe_title, recipe_category = :recipe_category, recipe_content = :recipe_content where recipe_id = :recipe_id";
 	$statement = $dbh -> prepare($sql);
   $statement -> bindParam(':recipe_id', $recipe_title, PDO::PARAM_INT);
@@ -19,17 +18,49 @@ try{
 	$statement -> bindParam(':recipe_category', $recipe_category, PDO::PARAM_INT);
 	$statement -> bindParam(':recipe_content', $recipe_content, PDO::PARAM_STR);
 	//$statement -> bindParam(':recipe_image', $recipe_image, PDO::PARAM_STR);
-	$statement -> execute();*/
+	$statement -> execute();
 
   $result = getIngredient($recipe_id);
   $rowResult = $result -> fetchAll();
 
   if(count($recipe_ingredients) > count($rowResult)){
-    echo count($recipe_ingredients).">".count($rowResult);
+    for ($i = 0; $i < count($rowResult); $i++) {
+      $sql = "update ingredient set ingredient_name = :ingredient_name, ingredient_amount = :ingredient_amount where ingredient_id = :ingredient_id";
+    	$statement = $dbh -> prepare($sql);
+      $statement -> bindParam(':ingredient_id', $recipe_ingredients[$i][0], PDO::PARAM_INT);
+      $statement -> bindParam(':ingredient_name', $recipe_ingredients[$i][1], PDO::PARAM_STR);
+      $statement -> bindParam(':ingredient_amount', $recipe_ingredients[$i][2], PDO::PARAM_STR);
+    	$statement -> execute();
+      echo $recipe_ingredients[$i][0].",".$recipe_ingredients[$i][1].",".$recipe_ingredients[$i][2]."/";
+    }
+    for ($j = count($rowResult); $j < count($recipe_ingredients); $j++) {
+      $sql = "insert into ingredient (member_id, recipe_id, ingredient_name, ingredient_amount) values(:member_id, :recipe_id, :ingredient_name, :ingredient_amount)";
+    	$statement = $dbh -> prepare($sql);
+      $statement -> bindParam(':member_id', $member_id, PDO::PARAM_INT);
+      $statement -> bindParam(':recipe_id', $recipe_id, PDO::PARAM_INT);
+      $statement -> bindParam(':ingredient_name', $recipe_ingredients[$j][1], PDO::PARAM_STR);
+      $statement -> bindParam(':ingredient_amount', $recipe_ingredients[$j][2], PDO::PARAM_STR);
+    	$statement -> execute();
+      echo $recipe_ingredients[$j][0].",".$recipe_ingredients[$j][1].",".$recipe_ingredients[$j][2]."/";
+    }
   } else if (count($recipe_ingredients) < count($rowResult)) {
-    echo count($recipe_ingredients)."<".count($rowResult);
+    $sql = "delete from ingredient where recipe_id = :recipe_id";
+  	$statement = $dbh -> prepare($sql);
+    $statement -> bindParam(':recipe_id', $recipe_id, PDO::PARAM_INT);
+  	$statement -> execute();
+
+    for ($j = 0; $j < count($recipe_ingredients); $j++) {
+      $sql = "insert into ingredient (member_id, recipe_id, ingredient_name, ingredient_amount) values(:member_id, :recipe_id, :ingredient_name, :ingredient_amount)";
+    	$statement = $dbh -> prepare($sql);
+      $statement -> bindParam(':member_id', $member_id, PDO::PARAM_INT);
+      $statement -> bindParam(':recipe_id', $recipe_id, PDO::PARAM_INT);
+      $statement -> bindParam(':ingredient_name', $recipe_ingredients[$j][1], PDO::PARAM_STR);
+      $statement -> bindParam(':ingredient_amount', $recipe_ingredients[$j][2], PDO::PARAM_STR);
+    	$statement -> execute();
+      echo $recipe_ingredients[$j][0].",".$recipe_ingredients[$j][1].",".$recipe_ingredients[$j][2]."/";
+    }
   } else{
-    for ($i=0; $i < count($recipe_ingredients); $i++) {
+    for ($i = 0; $i < count($recipe_ingredients); $i++) {
       $sql = "update ingredient set ingredient_name = :ingredient_name, ingredient_amount = :ingredient_amount where ingredient_id = :ingredient_id";
     	$statement = $dbh -> prepare($sql);
       $statement -> bindParam(':ingredient_id', $recipe_ingredients[$i][0], PDO::PARAM_INT);
--- www/js/recipeController.js
+++ www/js/recipeController.js
@@ -35,7 +35,7 @@ angular.module('starter.recipeController', [])
     $state.go("app.recipe_edit." + tabs);
   }
 
-  $scope.deleteRecipe = function () {
+  $scope.deleteRecipe = function() {
     $http.post(serverIP + "/cook/api/recipe_delete.php", {
         "recipe_id": current_recipe_id
       })
@@ -61,8 +61,21 @@ angular.module('starter.recipeController', [])
     }
   }, 100);
 
+  $scope.checkIfLastRow = function() {
+    var last_ingredient_edit_index = $scope.recipe_edit.recipe_ingredients.length - 1;
+    var last_ingredient_edit_id = "ingredient_edit" + last_ingredient_edit_index;
+    if (event.currentTarget.id == last_ingredient_edit_id) {
+      $scope.recipe_edit.recipe_ingredients.push(["", "", ""]);
+      //console.log("last");
+    }
+  }
+
   $scope.saveRecipe = function() {
+    var ingredient_filter = $scope.recipe_edit.recipe_ingredients.filter(function(item) {
+      return (item[1] !== "");
+    });
     console.log($scope.recipe_edit);
+    console.log(ingredient_filter);
 
     $http.post(serverIP + "/cook/api/recipe_edit.php", {
         "member_id": user_data.member_id,
@@ -71,12 +84,14 @@ angular.module('starter.recipeController', [])
         "recipe_category": $scope.recipe_edit.recipe_category,
         "recipe_content": $scope.recipe_edit.recipe_content,
         "recipe_image": $scope.recipe_edit.recipe_image,
-        "recipe_ingredients": $scope.recipe_edit.recipe_ingredients
+        "recipe_ingredients": ingredient_filter
       })
       .success(function(data, status, headers, config) {
         console.log(data);
-        var new_recipe_data = recipe_data[current_recipe_id];
+        var new_recipe_data = recipe_data;
         new_recipe_data[current_recipe_id] = $scope.recipe_edit;
+        new_recipe_data[current_recipe_id].recipe_ingredients = ingredient_filter;
+        console.log(new_recipe_data);
         $localstorage.setObject('recipe_data', new_recipe_data);
         //$state.go("app.home");
       })
@@ -84,8 +99,18 @@ angular.module('starter.recipeController', [])
   ionicMaterialInk.displayEffect();
 })
 
-.controller('RecipeAddCtrl', function($scope, $state, $http, $timeout, $localstorage, $ionicPopup, $ionicLoading, ionicMaterialInk) {
+.controller('RecipeAddCtrl', function($scope, $state, $http, $timeout, $localstorage, $ionicPopup, $ionicLoading, $cordovaFile, ionicMaterialInk) {
   $scope.recipe_add = {};
+  $scope.recipe_add.recipe_ingredients = [{
+    "name": "",
+    "amount": ""
+  }, {
+    "name": "",
+    "amount": ""
+  }, {
+    "name": "",
+    "amount": ""
+  }];
   $http.get(serverIP + "/cook/api/getCategory.php")
     .success(function(data, status, headers, config) {
       $scope.category_list = data;
@@ -93,6 +118,9 @@ angular.module('starter.recipeController', [])
     })
 
   $scope.submitRecipe = function() {
+    var ingredient_filter = $scope.recipe_add.recipe_ingredients.filter(function(item) {
+      return (item.name !== "");
+    });
     console.log($scope.recipe_add);
 
     $http.post(serverIP + "/cook/api/recipe_add.php", {
@@ -101,11 +129,84 @@ angular.module('starter.recipeController', [])
         "recipe_category": $scope.recipe_add.recipe_category,
         "recipe_content": $scope.recipe_add.recipe_content,
         "recipe_image": $scope.recipe_add.recipe_image,
-        "recipe_ingredients": $scope.recipe_add.recipe_ingredients
+        "recipe_ingredients": ingredient_filter
       })
       .success(function(data, status, headers, config) {
         console.log(data);
+        $scope.recipe_add = null;
         $state.go("app.home");
       })
   }
+
+  $scope.checkIfLastRow = function() {
+    var last_ingredient_index = $scope.recipe_add.recipe_ingredients.length - 1;
+    var last_ingredient_id = "ingredient" + last_ingredient_index;
+    if (event.currentTarget.id == last_ingredient_id) {
+      $scope.recipe_add.recipe_ingredients.push({
+        "name": "",
+        "amount": ""
+      });
+      //console.log("last");
+    }
+  }
+
+  $scope.takePic = function() {
+    var options = {
+      quality: 50,
+      destinationType: Camera.DestinationType.FILE_URI,
+      sourceType: 1, // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+      encodingType: 0 // 0=JPG 1=PNG
+    }
+    navigator.camera.getPicture(onSuccess, onFail, options);
+  }
+  var onSuccess = function(FILE_URI) {
+    console.log(FILE_URI);
+    $scope.picData = FILE_URI;
+    $scope.$apply();
+  };
+  var onFail = function(e) {
+    console.log("On fail " + e);
+  }
+  $scope.send = function() {
+    var myImg = $scope.picData;
+    var options = new FileUploadOptions();
+    options.fileKey = "post";
+    options.chunkedMode = false;
+    var params = {};
+    params.user_token = localStorage.getItem('auth_token');
+    params.user_email = localStorage.getItem('email');
+    options.params = params;
+    var ft = new FileTransfer();
+    ft.upload(myImg, encodeURI("https://example.com/posts/"), onUploadSuccess, onUploadFail, options);
+  }
 })
+
+// Photo Upload Test
+
+/*
+<div role="main" class="ui-content">
+<a id="camera" class="ui-btn">拍照</a>
+<a id="upload" class="ui-btn">上傳</a>
+<ul id="list" data-role="listview" data-count-theme="b" data-inset="true">
+  <li>目前尚未上傳數量
+    <span class="ui-li-count">0</span>
+  </li>
+</ul>
+</div>
+
+.controller("PhotoUploadCtrl", function($scope, $cordovaFile){
+  $scope.upload = function(){
+    var options = {
+        fileKey: "",
+        fileName: "",
+        chunkedMode: ,
+        mimeType: ""
+    };
+    $cordovaFile.uploadFile("http://140.123.175.102:8080/file/upload", "/anddroid_asset/www/img/ionic.png", options).then(function(result){
+      console.log("success: " + JSON.stringify(result.response));
+    }, function(error){
+      console.log("error: " + error);
+    });
+  }
+})
+*/
